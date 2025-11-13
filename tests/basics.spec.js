d49f3af8653c2da4f8b0f26064459a397cb25897
Poojitha <jupitha123@gmail.com>
2025-11-13 13:06:53 +0530

    new changes

--- tests/basics.spec.js
+++ tests/basics.spec.js
@@ -1,6 +1,6 @@
 const {test, expect} = require('@playwright/test')
 
-test.only('basic test', async ({browser})=>
+test('basic test', async ({browser})=>
 {
     const context = await browser.newContext();
     const page = await context.newPage();
